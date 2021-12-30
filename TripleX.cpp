5f629c42f98f91af0122979b14156fd5e7b32ffc
Michal Kasiarz <michal.kasiarz@protonmail.com>
2021-12-30 06:59:56 +0100

    if/else statement added, cin function

--- TripleX.cpp
+++ TripleX.cpp
@@ -2,25 +2,41 @@
 
 int main()
 {
-    // Expression statements
     std::cout << "You are a secret agent breaking into the server room...";
     std::cout << std::endl;
     std::cout << "Enter the code..." << std::endl;
 
-    // Declaration statements
     const int CodeA = 4;
     const int CodeB = 2;
-    const int CodeC = 9;
+    const int CodeC = 1;
 
     const int CodeSum = CodeA + CodeB + CodeC;
     const int CodeProduct = CodeA * CodeB * CodeC;
 
-    // Expression statements
     std::cout << std::endl;
     std::cout << "+ There are 3 numbers in the code" << std::endl;
     std::cout << "+ The codes add-up to: " << CodeSum << std::endl;
     std::cout << "+ The codes multiply to give: " << CodeProduct << std::endl;
 
-    // Return statement
+    std::cout << std::endl;
+
+    int GuessA, GuessB, GuessC;
+
+    std::cin >> GuessA;
+    std::cin >> GuessB;
+    std::cin >> GuessC;
+    std::cout << "Your guess: " << GuessA << GuessB << GuessC;
+
+    int GuessSum = GuessA + GuessB + GuessC;
+    int GuessProduct = GuessA * GuessB * GuessC;
+
+    std::cout << std::endl;
+
+    if (GuessSum == CodeSum && GuessProduct == CodeProduct) {
+        std::cout << "You win!" << std::endl;
+    } else {
+        std::cout << "You lost!" << std::endl;
+    }
+
     return 0;
 }
